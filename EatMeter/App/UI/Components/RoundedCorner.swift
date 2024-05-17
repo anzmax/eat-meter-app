20abde0ba0cfdfeccb1117b926cfd77c483b4ce2
anzmax <anzmaksimovic@gmail.com>
2024-05-17 16:37:07 +0100

    added basic layout of the main screen

---
+++ EatMeter/App/UI/Components/RoundedCorner.swift
@@ -0,0 +1,31 @@
+//
+//  RoundedCorner.swift
+//  EatMeter
+//
+//  Created by Lika Maksimovic on 17.05.2024.
+//
+
+import SwiftUI
+
+struct RoundedCorner: Shape {
+    private let radius: CGFloat
+    private let corners: UIRectCorner
+
+    init(
+        radius: CGFloat = .infinity,
+        corners: UIRectCorner = .allCorners
+    ) {
+        self.radius = radius
+        self.corners = corners
+    }
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(
+            roundedRect: rect,
+            byRoundingCorners: corners,
+            cornerRadii: CGSize(width: radius, height: radius)
+        )
+        return Path(path.cgPath)
+    }
+}
+
